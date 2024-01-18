ec9d9942b18b8810a2ced0c666af6f01679a9f2d
Alexandr Zuev <N2FBpJDqu7cQdyP>
2024-01-18 22:33:23 +0300

    Проект №14 Финальная версия

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,20 @@
 const jwt = require('jsonwebtoken');
 
-const UNAUTHORIZED = 401;
 const SECRET_KEY = '123';
 
 function authMiddleware(req, res, next) {
   const token = req.cookies.jwt;
   if (!token) {
-    return res.status(UNAUTHORIZED).json({ message: 'Токен авторизации отсутствует' });
+    const error = new Error();
+    error.status = 401;
+    throw error;
   }
   try {
     const payload = jwt.verify(token, SECRET_KEY);
     req.user = payload;
     return next();
-  } catch (error) {
-    return res.status(UNAUTHORIZED).json({ message: 'Ошибка авторизации: недействительный токен' });
+  } catch (err) {
+    return next(err);
   }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,21 @@
 const router = require('express').Router();
 const {
+  celebrate, Joi, Segments,
+} = require('celebrate');
+const {
   getUsers, getUserById, updateProfile, updateAvatar, getMyProfile,
 } = require('../controllers/users');
+
+const avatarUpdateValidation = celebrate({
+  [Segments.BODY]: {
+    avatar: Joi.string().uri(),
+  },
+});
 
 router.get('/', getUsers);
 router.get('/me', getMyProfile);
 router.get('/:userId', getUserById);
 router.patch('/me', updateProfile);
-router.patch('/me/avatar', updateAvatar);
+router.patch('/me/avatar', avatarUpdateValidation, updateAvatar);
 
 module.exports = router;
